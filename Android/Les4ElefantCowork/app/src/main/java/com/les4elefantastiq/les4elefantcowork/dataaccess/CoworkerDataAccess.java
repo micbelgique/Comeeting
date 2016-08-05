353febdcc43aebaae633908f1c5eb64f40447eec
Mathias <matsou03@gmail.com>
2016-08-05 19:03:22 +0200

    Coworker model + retrofit WIP

---
+++ Android/Les4ElefantCowork/app/src/main/java/com/les4elefantastiq/les4elefantcowork/dataaccess/CoworkerDataAccess.java
@@ -0,0 +1,63 @@
+package com.les4elefantastiq.les4elefantcowork.dataaccess;
+
+import com.les4elefantastiq.les4elefantcowork.models.Coworker;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by Math on 05/08/16.
+ */
+public class CoworkerDataAccess {
+
+    public static final String API_URL = "http://comeetingapi.azurewebsites.net";
+
+    public static Coworker getProfile(Coworker coworker) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        CoworkerInterface coworkerInterface = retrofit.create(CoworkerInterface.class);
+        Call<Coworker> coworkerCall = coworkerInterface.getProfile(coworker.linkedInId);
+
+        try {
+            return coworkerCall.execute().body();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public static void login(Coworker coworker) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        CoworkerInterface coworkerInterface = retrofit.create(CoworkerInterface.class);
+        Call<Void> loginCall = coworkerInterface.login(coworker);
+
+        try {
+            loginCall.execute().body();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public interface CoworkerInterface {
+        @POST("/api/coworker")
+        Call<Void> login(@Path("coworker") Coworker coworker);
+
+        @GET("/api/coworker/{linkedInId}")
+        Call<Coworker> getProfile(@Path("linkedInId") String linkedInId);
+    }
+}
