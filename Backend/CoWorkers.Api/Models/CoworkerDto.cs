6088c9abb87544f6f14da10d81ead4b68b222261
Laurent Vandenbosch <laurent@agilys.be>
2016-08-05 14:57:10 +0200

    Initial commit with first API method mocked

---
+++ Backend/CoWorkers.Api/Models/CoworkerDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Comeeting.Api.Models
+{
+    public class CoworkerDto
+    {
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+        [JsonProperty("pictureUrl")]
+        public string PictureUrl { get; set; }
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+    }
+}
---
+++ Backend/CoWorkers.Domain/Coworkspace.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comeeting.Domain
+{
+    public class Coworkspace
+    {
+        public string Name { get; set; }
+        public string PictureUrl { get; set; }
+        public string Description { get; set; }
+        public double GeolocationLongitude { get; set; }
+        public double GeolocationLatitude { get; set; }
+        public int GeofencingRadius { get; set; }
+
+        public virtual IEnumerable<Coworker> Coworkers { get; set; }
+
+    }
+}
