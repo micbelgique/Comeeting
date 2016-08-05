c00bc8e8420cff19f420404e09165922715778c6
Valentin Taleb <talebvalentin@gmail.com>
2016-08-05 12:28:40 +0200

    First commit of the Android Project

---
+++ Android/Les4ElefantCowork/app/src/main/java/com/les4elefantastiq/les4elefantcowork/activites/MainActivity.java
@@ -0,0 +1,16 @@
+package com.les4elefantastiq.les4elefantcowork.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.les4elefantastiq.les4elefantcowork.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
