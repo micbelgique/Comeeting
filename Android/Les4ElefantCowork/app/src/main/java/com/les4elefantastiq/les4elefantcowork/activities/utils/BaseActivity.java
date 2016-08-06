0f54fe6951fb19cfaa1cadfd1bc680e4a87621bb
Valentin Taleb <talebvalentin@gmail.com>
2016-08-07 00:46:28 +0200

    no message

--- Android/Les4ElefantCowork/app/src/main/java/com/les4elefantastiq/les4elefantcowork/activities/utils/BaseActivity.java
+++ Android/Les4ElefantCowork/app/src/main/java/com/les4elefantastiq/les4elefantcowork/activities/utils/BaseActivity.java
@@ -1,11 +1,8 @@
 package com.les4elefantastiq.les4elefantcowork.activities.utils;
 
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.les4elefantastiq.les4elefantcowork.R;
 
@@ -24,13 +21,6 @@ public class BaseActivity extends AppCompatActivity {
     protected void manageToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
-        }
-
     }
 
 
