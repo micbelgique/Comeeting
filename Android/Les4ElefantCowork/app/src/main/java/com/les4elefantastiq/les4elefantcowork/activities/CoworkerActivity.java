772b0d01b3c8b8b51b79417ccaf0773c86c5bd4b
Valentin Taleb <talebvalentin@gmail.com>
2016-08-07 11:11:21 +0200

    no message

--- Android/Les4ElefantCowork/app/src/main/java/com/les4elefantastiq/les4elefantcowork/activities/CoworkerActivity.java
+++ Android/Les4ElefantCowork/app/src/main/java/com/les4elefantastiq/les4elefantcowork/activities/CoworkerActivity.java
@@ -87,6 +87,7 @@ public class CoworkerActivity extends BaseActivity {
             if (coworker != null) {
                 ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar)).setTitle(coworker.firstName + " " + coworker.lastName);
                 ((TextView) findViewById(R.id.textview_summary)).setText(coworker.summary);
+                ((TextView) findViewById(R.id.textview_linked_in_title)).setText("Profil LinkedIn de " + coworker.firstName);
 
                 Picasso.with(getBaseContext())
                         .load(coworker.pictureUrl)
