a8bd8b27209c3261ff46311133010f9fbd39b459
Mathias <matsou03@gmail.com>
2016-08-23 10:16:27 +0200

    Add a screenshot

---
+++ Android/Les4ElefantCowork/app/src/main/java/com/les4elefantastiq/les4elefantcowork/models/Coworkers.java
@@ -0,0 +1,15 @@
+package com.les4elefantastiq.les4elefantcowork.models;
+
+import java.util.List;
+
+/**
+ * Created by Math on 07/08/16.
+ */
+public class Coworkers {
+
+    public List<Coworker> coworkers;
+
+    public Coworkers(List<Coworker> coworkers) {
+        this.coworkers = coworkers;
+    }
+}
---
+++ Android/Les4ElefantCowork/app/src/main/java/com/les4elefantastiq/les4elefantcowork/models/LiveFeedMessages.java
@@ -0,0 +1,29 @@
+package com.les4elefantastiq.les4elefantcowork.models;
+
+import android.support.annotation.NonNull;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class LiveFeedMessages {
+
+    // -------------- Objects, Variables -------------- //
+
+    public List<LiveFeedMessage> liveFeedMessages;
+
+
+    // ----------------- Constructor ------------------ //
+
+    public LiveFeedMessages(List<LiveFeedMessage> liveFeedMessages) {
+        this.liveFeedMessages = liveFeedMessages;
+    }
+
+
+    // ---------------- Public Methods ---------------- //
+
+    // ---------------- Private Methods --------------- //
+
+    // ----------------- Miscellaneous ---------------- //
+}
